7373e3019705eb1bba0c882b21d7dc11f11bf552
Ricardo Martinez <ricardo@smartpaymx.com>
2024-07-04 16:01:01 -0600

    Initial commit

---
+++ DateTimesSamples/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DateTimesSamples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var zoneId = TimeZoneInfo.Local.Id;
+        // string zoneId = "Central Standard Time";
+        TimeZoneInfo zone = TimeZoneInfo.FindSystemTimeZoneById(zoneId);
+    }
+    
+}
+
+public class CustomDateTime
+{
+    public static DateTime ToLocalTime(DateTime time)
+    {
+        if (time.Kind == DateTimeKind.Local)
+        {
+            return time;
+        }
+        else if (time.Kind == DateTimeKind.Utc)
+        {
+            DateTime returnTime = new DateTime(time.Ticks, DateTimeKind.Local);
+            returnTime += GetUtcOffset(returnTime);
+            if (TimeZoneInfo.Local.IsDaylightSavingTime(returnTime))
+                returnTime -= new TimeSpan(1, 0, 0);
+            return returnTime;
+        }      
+        else
+        {
+            throw new ArgumentException("The source time zone cannot be determined.");
+        }      
+    }
+    
+    private static TimeSpan GetUtcOffset(DateTime time)
+    {
+        return TimeZoneInfo.Local.GetUtcOffset(time);
+    }
+}
+
+
---
+++ UserValidation/Views/Home/Account/ModelBinding.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserValidation.Views.Home.Account;
+
+public class ModelBinding : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public void OnPost()
+    {
+    }
+}
+public class Credential
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+        
+}
