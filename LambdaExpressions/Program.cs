d34034a1154aca926b98d913fa8a5a928eb5845f
Ricardo Martinez <ricardo@smartpaymx.com>
2024-07-10 22:42:46 -0600

    adding fnHigherOrder

--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Func<int, Func<int, int>, int> fnHigherOrder = (number, fn) =>
+{
+    if (number > 100)
+    {
+        return fn(number);
+    }
 
-Console.WriteLine("Hello, World!");
+    return number;
+};
+var result = fnHigherOrder(200, number => number * 2);
+Console.WriteLine(result);
+
+return;
+int Summary(int numerator, int denominator) => numerator > denominator ? numerator : denominator;
+decimal Suma(int a, int b) => a + b;
