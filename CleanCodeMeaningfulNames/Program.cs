9df2b0a79d76e96c9d8097ec14bc7a400881ca76
Ricardo Martinez <ricardo@smartpaymx.com>
2024-07-29 20:25:56 -0600

    adding flagged method

---
+++ CleanCodeMeaningfulNames/Program.cs
@@ -0,0 +1,28 @@
+﻿
+var flaggedCellResult = GetFlaggedCells([
+    new Cell(),
+    new Cell()
+]);
+
+Console.WriteLine(flaggedCellResult.Count);
+return;
+
+
+List<Cell> GetFlaggedCells(List<Cell> gameBoard)
+{
+    return gameBoard.Where(cell => cell.IsFlagged()).ToList();
+    // when is static method
+    return gameBoard.Where(_ => Cell.IsFlaggedStatic()).ToList();
+}
+internal class Cell
+{
+    public bool IsFlagged()
+    {
+        return true;
+    }
+    
+    public static bool IsFlaggedStatic()
+    {
+        return true;
+    }
+}
