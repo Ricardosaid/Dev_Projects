602f2978c4b86f0d43be1069204014479285d0e7
Ricardo Martinez <ricardo@smartpaymx.com>
2024-07-29 20:04:39 -0600

    adding windows functions

---
+++ WindowsFunctions/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using System.Runtime.InteropServices;
+Windows.MessageBox(IntPtr.Zero, "Hello World!", "Hello Dialog", 0);
+public class Windows
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+}
