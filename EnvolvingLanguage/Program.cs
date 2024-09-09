b7ee161786d1e298b0bfb2f02945d0463b0d9dc0
Ricardo Martinez <ricardo@smartpaymx.com>
2024-09-08 22:10:11 -0600

    excel reader project init

---
+++ EnvolvingLanguage/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var products = new List<Product>
+{
+    new Product { Name = "Laptop", Price = 1000, Brand = "Dell", Category = "Electronics", Description = "Dell Laptop" },
+    new Product { Name = "Mobile", Price = 500, Brand = "Samsung", Category = "Electronics", Description = "Samsung Mobile" },
+    new Product { Name = "Shirt", Price = 50, Brand = "Levis", Category = "Clothing", Description = "Levis Shirt" },
+    new Product { Name = "T-Shirt", Price = 20, Brand = "Nike", Category = "Clothing", Description = "Nike T-Shirt" },
+    new Product { Name = "Shoes", Price = 100, Brand = "Adidas", Category = "Footwear", Description = "Adidas Shoes" },
+    new Product { Name = "Sneakers", Price = 80, Brand = "Reebok", Category = "Footwear", Description = "Reebok Sneakers" }
+};
+
+
+var productQuery = 
+    from prod in products
+    select  new {prod.Description, prod.Price};
+
+foreach (var v in productQuery)
+{
+    Console.WriteLine("Cost of {0} is {1}", v.Description, v.Price);
+    
+}
+
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+}
---
+++ ExcelReader/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
+using ExcelDataReader;
+
+string filePath = @"C:\Users\said_\Downloads\Results.xlsx";
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+// Reading from a .xls file (Excel 97-2003)
+using var reader = ExcelReaderFactory.CreateReader(stream);
+// Use the AsDataSet method to read the data into a DataSet
+var result = reader.AsDataSet();
+
+// Assuming the data is on the first sheet
+var dataTable = result.Tables[0];
+
+// Print the data
+Console.WriteLine("Card Number\tAmount");
+foreach (DataRow row in dataTable.Rows)
+{
+    // Assuming the cardNumber is in the first column (index 0)
+    // and amount is in the second column (index 1)
+    string cardNumber = row[0].ToString();
+    string amount = row[1].ToString();
+
+    Console.WriteLine($"{cardNumber}\t{amount}");
+}
