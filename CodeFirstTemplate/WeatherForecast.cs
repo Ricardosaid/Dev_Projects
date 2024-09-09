edec6840c6c49b98c74db5680a7cb1bb397db027
Ricardo Martinez <ricardo@smartpaymx.com>
2024-09-08 22:08:50 -0600

    adding codefirst project

---
+++ CodeFirstTemplate/WeatherForecast.cs
@@ -0,0 +1,19 @@
+namespace CodeFirstTemplate;
+
+public class WeatherForecast
+{
+    private string name;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
