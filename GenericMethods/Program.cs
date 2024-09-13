f8052b20728c48df45b31d313234f62a38f1effe
Ricardo Martinez <ricardo@smartpaymx.com>
2024-09-12 22:54:20 -0600

    adding generic class and methods

---
+++ GenericMethods/Program.cs
@@ -0,0 +1,50 @@
+﻿// //Generic methods
+//
+// static List<T> CopyAtMost<T>(List<T> input, int maxElements)
+// {
+//     var actualCount = Math.Min(input.Count, maxElements);
+//     var ret = new List<T>();
+//     for (var i = 0; i < actualCount; i++)
+//     {
+//         ret.Add(input[i]);
+//     }
+//     return ret;
+// }
+//
+// Initializer();
+// return;
+//
+// static void Initializer()
+// {
+//     List<int> numbers =
+//     [
+//         1,
+//         2,
+//         3
+//
+//     ];
+//
+//     var firstTwo = CopyAtMost(numbers, 2);
+//     Console.WriteLine(firstTwo.Count); 
+// }
+
+
+List<int> values = new List<int>
+{
+    1, 2, 3, 4, 5, 6
+};
+
+var newList = CopyAtMost(values, 3);
+Console.WriteLine(newList.Count);
+
+static List<T> CopyAtMost<T>(List<T> input, int maxElements)
+{
+    var actualCount = Math.Min(input.Count, maxElements);
+    var ret = new List<T>();
+    for (var i = 0;  i < maxElements; i++)
+    {
+        ret.Add(input[i]);
+    }
+
+    return ret;
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
 
-Console.WriteLine("Hello, World!");
+// ArrayList is a non-generic collection
+var names = GenerateNames();
+PrintNames(names);
+static ArrayList GenerateNames()
+{
+    ArrayList names = new ArrayList();
+    names.Add("Ricardo");
+    names.Add("Mauricio");
+    names.Add("Daniel");
+    names.Add(new Exception());
+    return names;
+}
+
+static void PrintNames(ArrayList names)
+{
+    foreach (string name in names)
+    {
+        Console.WriteLine(name);
+    }
+    
+}
+
+
+// Generic class
+List<string> list = new List<string>();
+list.Add("Ricardo");
+list.Brand = "Apple";
+list.Name = "iPhone";
+if (list.Name.Length > 0)
+{
+    Console.WriteLine("Name is not empty");
+}
+
+Console.WriteLine(list);
+
+internal class List<T>
+{
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public void Add(T item)
+    {
+        Console.WriteLine("Adding item");
+        Console.WriteLine(item);
+    }
+}
