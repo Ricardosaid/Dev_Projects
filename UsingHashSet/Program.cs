bedddc37852d3ed9c0ccba0bc9ec4a07dc45883c
Ricardo Martinez <ricardo@smartpaymx.com>
2025-01-17 13:33:13 -0600

    using hashing methods to sort values

---
+++ UsingHashSet/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace UsingHashSet;
+
+class Union
+{
+    public void SetOperationUnion()
+    {
+        HashSet<int> setUnion = new HashSet<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            setUnion.Add(i);
+        }
+        
+        HashSet<int> setUnion2 = new HashSet<int>();
+        
+        for (int i = 5; i < 15; i++)
+        {
+            setUnion2.Add(i);
+        }
+        
+        setUnion.UnionWith(setUnion2);
+        
+        foreach (int i in setUnion)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Union union = new Union();
+        union.SetOperationUnion();
+    }
+}
